4edfbe3fd53c3468a525a6f746e02ba2ad828fef
chiscookeke11 <chiscookeke11@gmail.com>
2025-07-08 15:27:23 +0100

    uodate

---
+++ lagos-rhythm/src/app/VirtualTour/page.tsx
@@ -0,0 +1,14 @@
+import ChooseTour from "@/components/VirtualTour/ChooseTour";
+import VirtualTourHero from "@/components/VirtualTour/VirtualTourHero";
+
+
+
+
+export default function Page() {
+    return (
+        <div className="w-full" >
+            <VirtualTourHero/>
+            <ChooseTour/>
+        </div>
+    )
+}
