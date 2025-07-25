3ba388aa443a07533f87ab6171e944805876e835
Chinedu Okeke <chiscookeke11@gmail.com>
2025-07-25 07:30:02 +0100

    udpaytes

---
+++ src/components/BlogsSection.tsx
@@ -0,0 +1,14 @@
+import BlogCard from "./common/BlogCard";
+
+
+
+
+
+
+export default function BlogSection() {
+    return (
+        <section className=" w-full mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 " >
+<BlogCard/>
+        </section>
+    )
+}
