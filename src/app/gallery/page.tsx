92d68c7c3e82b00db82e14ba5b991106c25f3739
Chinedu Okeke <chiscookeke11@gmail.com>
2025-07-28 15:17:28 +0100

    added the next and prev buttons for the gallery

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -1,6 +1,7 @@
 "use client"
 import { pagesData } from "@/data/data";
 import { AnimatePresence, motion } from "framer-motion";
+import { ChevronLeft, ChevronRight } from "lucide-react";
 import Image from "next/image";
 import { useEffect, useState } from "react";
 
@@ -29,20 +30,37 @@ export default function Page() {
     }, [showFrame])
 
 
+    const handleNextFrame = () => {
+        if (selectedFrame >= pagesData.length - 1) {
+            return
+        }
+        setSelectedFrame((prev) => prev + 1)
+    }
+
+
+    const handlePrevFrame = () => {
+        if (selectedFrame <= 0) {
+            return
+        }
+        setSelectedFrame((prev) => prev - 1)
+    }
 
 
     function ImagePreview({ image, text }: { image: string; text: string }) {
         return (
             <motion.div
                 onClick={() => setShowFrame(false)}
-                className="bg-transparent backdrop-blur-2xl w-full h-screen flex items-center justify-center fixed p-5 z-10"
+                className="bg-transparent backdrop-blur-2xl w-full h-screen flex items-center justify-center fixed  z-10"
                 initial={{ opacity: 0 }}
                 animate={{ opacity: 1 }}
                 exit={{ opacity: 0 }}
             >
                 <motion.div
+                    onClick={(e) => {
+                        e.stopPropagation()
+                    }}
                     title={text}
-                    className="w-full max-w-2xl h-full max-h-10/12 flex items-center justify-center relative bg-white rounded-lg overflow-hidden"
+                    className="w-full mx-auto max-w-2xl h-full max-h-10/12 flex items-center justify-center relative bg-white rounded-lg overflow-hidden"
                     initial={{ scale: 0.8, opacity: 0 }}
                     animate={{ scale: 1, opacity: 1 }}
                     exit={{ scale: 0.8, opacity: 0 }}
@@ -57,7 +75,12 @@ export default function Page() {
                     />
                     <div className="absolute bottom-0 left-0 w-full h-fit bg-black/40 backdrop-blur-sm py-4 px-3">
                         <p className="text-base font-bold font-merriweather text-white">{text}</p>
+
+
                     </div>
+                    <button disabled={selectedFrame >= pagesData.length - 1} className="absolute top-[50%] translate-y-[-50%] right-0 cursor-pointer bg-[#EF8F57]/80 w-10 h-10 rounded-full flex items-center justify-center disabled:bg-neutral-400 " onClick={handleNextFrame} ><ChevronRight />  </button>
+                    <button disabled={selectedFrame <= 0} className="absolute top-[50%] translate-y-[-50%] left-0 cursor-pointer bg-[#EF8F57]/80 w-10 h-10 rounded-full flex items-center justify-center disabled:bg-neutral-400 " onClick={handlePrevFrame} ><ChevronLeft /> </button>
+
                 </motion.div>
             </motion.div>
         );
@@ -69,7 +92,7 @@ export default function Page() {
         <div className="bg-[#05073C] w-full h-full flex items-center justify-center text-black  " >
 
 
-            <div className="w-full h-fit grid md:grid-cols-2 lg:grid-cols-4 grid-rows-2 gap-3 py-20  m-4 lg:m-8  cursor-pointer relative " >
+            <div className="w-full h-fit grid md:grid-cols-2 lg:grid-cols-4 grid-rows-2 gap-3 py-20    cursor-pointer relative " >
                 {
                     pagesData.map((card, index) => (
                         <div
@@ -87,7 +110,7 @@ export default function Page() {
                 }
 
                 <AnimatePresence>
-                    {showFrame &&
+                    {showFrame && pagesData[selectedFrame] &&
                         (<ImagePreview
                             image={pagesData[selectedFrame].image}
                             text={pagesData[selectedFrame].text}
