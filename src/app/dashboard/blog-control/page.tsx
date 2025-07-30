dec2a08548607f121af5ecdd86af506c87c40dd3
Chinedu Okeke <chiscookeke11@gmail.com>
2025-07-30 18:58:18 +0100

    implemeneted the blog upload function for text

--- src/app/dashboard/blog-control/page.tsx
+++ src/app/dashboard/blog-control/page.tsx
@@ -1,10 +1,26 @@
+"use client"
+
+import BlogSection from "@/components/BlogsSection";
+import AddBlogModal from "@/components/dashboard/AddBlogModal";
+import { Button } from "@/components/ui/button";
+import { useState } from "react";
 
 
 
 export default function Page() {
+    const [openBlogModal, setOpenBlogModal] = useState(false)
+
+
+
     return (
-        <div>
-            blogs control
+        <div className="w-full flex flex-col gap-8 items-start justify-center py-6 px-7 relative font-lato ">
+          <Button onClick={() => setOpenBlogModal(true)} className="flex items-center justify-center gap-4 bg-[#EF8F57] cursor-pointer hover:bg-[#EF8F57]/90 py-3 px-6 h-fit text-base font-medium font-lato ml-auto " >Add Blog</Button>
+            <h1 className="text-[#05073C] font-bold text-2xl md:text-3xl font-playfair " >Blogs</h1>
+            <BlogSection />
+
+
+
+     {openBlogModal && (       <AddBlogModal/>)}
         </div>
     )
 }
