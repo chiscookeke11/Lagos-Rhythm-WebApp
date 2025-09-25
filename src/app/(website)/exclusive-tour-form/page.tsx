941176e449d4c29db6b85d6eccbd6535187d5af3
chiscookeke11 <chiscookeke11@gmail.com>
2025-09-25 13:49:03 +0100

    FEAT: exclusive tour form - added the isCompleted field to the db

--- src/app/(website)/exclusive-tour-form/page.tsx
+++ src/app/(website)/exclusive-tour-form/page.tsx
@@ -133,6 +133,7 @@ export default function Page() {
         paidPrice: paidPrice,
         populationSize: participantsCount,
         tourTheme: selectedTheme,
+        tourCompleted: false,
       })
 
 
