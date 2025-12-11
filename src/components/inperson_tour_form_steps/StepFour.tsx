1a20a3e070abfa21752aa2e992261eb307372281
chiscookeke11 <chiscookeke11@gmail.com>
2025-12-11 16:36:24 +0100

    inperson tour feature

--- src/components/inperson_tour_form_steps/StepFour.tsx
+++ src/components/inperson_tour_form_steps/StepFour.tsx
@@ -1,7 +1,11 @@
 import { inpersonFormUserData } from "@/Types/inpersonFormDataType";
 import Button from "../common/Button"
-import { SetStateAction } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { CustomCheckBox } from "../common/CustomCheckbox";
+import { useFlutterwave, closePaymentModal } from "flutterwave-react-v3"
+import { ThemeJourneyType } from "@/Types/ThemeJourneyType";
+import toast from "react-hot-toast";
+
 
 
 
@@ -11,10 +15,89 @@ import { CustomCheckBox } from "../common/CustomCheckbox";
 interface StepFourProps {
     formValues: inpersonFormUserData;
     setFormValues: React.Dispatch<React.SetStateAction<inpersonFormUserData>>;
-    setStepThreePositionX: React.Dispatch<SetStateAction<string>>;
+    packageDetails: ThemeJourneyType | null;
+    setStepThree: () => void;
+    onSuccessfulPayment: () => Promise<boolean>;
 }
 
-export default function StepFour({ formValues, setFormValues, setStepThreePositionX }: StepFourProps) {
+export default function StepFour({ formValues, setFormValues, packageDetails, setStepThree, onSuccessfulPayment }: StepFourProps) {
+    const flutterwavePublicKey = process.env.NEXT_PUBLIC_FLUTTERWAVE_API_KEY;
+    const [isProcessing, setIsProcessing] = useState(false)
+
+
+
+
+    if (!flutterwavePublicKey) {
+        console.error("NEXT_PUBLIC_FLUTTERWAVE_API_KEY is not defined")
+    }
+
+
+
+    const convertedPrice = formValues.price ? formValues.price  : 0
+
+
+    const config = {
+        public_key: flutterwavePublicKey || "",
+        tx_ref: `tx-${Date.now()}`,
+        amount: convertedPrice,
+        currency: "NGN",
+        payment_options: "card,mobilemoney,ussd",
+        customer: {
+            email: formValues.emailAddress,
+            phone_number: "",
+            name: formValues.fullName,
+        },
+        customizations: {
+            title: `THEME: ${packageDetails?.title} `,
+            description: packageDetails?.description ?? "",
+            logo: "https://res.cloudinary.com/dwedz2laa/image/upload/v1752824400/logo_ajy1ca.png",
+        },
+    }
+
+
+    const handleFlutterPayment = useFlutterwave(config)
+
+    const handleFiatPayment = useCallback(() => {
+        if (!flutterwavePublicKey) {
+            console.error("Cannot process payment: Flutterwave API key not configured")
+            return
+        }
+
+        setIsProcessing(true)
+    }, [flutterwavePublicKey])
+
+
+
+    useEffect(() => {
+        if (isProcessing) {
+            handleFlutterPayment({
+                callback: async (response) => {
+                    setIsProcessing(false)
+                    closePaymentModal()
+
+                    //  Verify status
+                    if (response.status === "completed") {
+                        console.log("Payment success:", response)
+
+                        //  Submit form to Firestore
+                        await onSuccessfulPayment()
+
+                        // Optional: Redirect user to success page
+                        // router.push("/success")
+                    } else {
+                        console.log("Payment failed:", response)
+                    }
+                },
+
+                onClose: () => {
+                    setIsProcessing(false)
+                }
+            })
+        }
+    }, [isProcessing, handleFlutterPayment])
+
+
+
 
 
     const handleCheckboxChange = (
@@ -44,34 +127,38 @@ export default function StepFour({ formValues, setFormValues, setStepThreePositi
         }
     };
 
-
+    const handleNext = () => {
+        if (!formValues.paymentType) {
+            toast.error("All required fields must be completed!")
+            return;
+        }
+        handleFiatPayment()
+    }
 
     return (
-        <div className={`flex items-center justify-center z-10  h-full w-full px-4 absolute top-[50%] left-[50%]  translate-y-[-50%] -translate-x-1/2 `} >
-            <div className=" bg-[#FDF4F1] h-fit w-full max-w-4xl  flex items-center justify-center py-4 px-3 flex-col gap-4 rounded-md  " >
+        <div className=" bg-[#FDF4F1] h-fit w-full max-w-2xl  flex items-center justify-center py-6 px-4 md:px-6 flex-col gap-7 rounded-md  " >
+            Payment
 
-                Payment
-
-                <div className="w-full flex flex-col items-start gap-5 " >
-                    <h1 className="text-[#000000] font-medium text-base font-lato flex items-start gap-1" >Select Payment Type:</h1>
+            <div className="w-full flex flex-col items-start gap-5 " >
+                <h1 className="text-[#000000] font-medium text-base font-lato flex items-start gap-1" >Select Payment Type:</h1>
 
 
-                    <div className="flex flex-wrap gap-4 justify-items-stretch  "  >
-                        {["Full Payment", "Deposit (50%)"].map((option, index) => {
-                            const isChecked = formValues.paymentType === option
-                            return (
-                                <CustomCheckBox
-                                    key={index}
-                                    checked={isChecked}
-                                    onCheckedChange={() => handleCheckboxChange("paymentType", true, option)}
-                                    label={option}
-                                    id={option}
+                <div className="flex flex-col  gap-4 justify-items-stretch  "  >
+                    {["Full Payment", "Deposit (50%)"].map((option, index) => {
+                        const isChecked = formValues.paymentType === option
+                        return (
+                            <CustomCheckBox
+                                key={index}
+                                checked={isChecked}
+                                onCheckedChange={() => handleCheckboxChange("paymentType", true, option)}
+                                label={option}
+                                id={option}
 
-                                />
-                            )
-                        })}
-                    </div>
+                            />
+                        )
+                    })}
                 </div>
+            </div>
 
 
 
@@ -85,29 +172,27 @@ export default function StepFour({ formValues, setFormValues, setStepThreePositi
 
 
 
-                {/* The navigation buttons  */}
-                <div className=" w-full flex items-center justify-between " >
+            {/* The navigation buttons  */}
+            <div className=" w-full flex items-center justify-between " >
 
-                    <Button
-                        label="Prev"
-                        type="button"
-                        ariaLabel="Previous"
-                        variant="ghost"
-                        className="!bg-[#EF8F57] w-fit rounded-sm"
-                        onClick={() => {
-                            setStepThreePositionX("translate-x-[-50%]")
-                        }}
-                    />
+                <Button
+                    label="Prev"
+                    type="button"
+                    ariaLabel="Previous"
+                    variant="ghost"
+                    className="!bg-[#EF8F57] w-fit rounded-sm"
+                    onClick={() => setStepThree()}
+                />
 
 
-                    <Button
-                        label="Submit"
-                        type="submit"
-                        ariaLabel="Submit"
-                        variant="ghost"
-                        className="!bg-[#EF8F57] w-fit rounded-sm"
-                    />
-                </div>
+                <Button
+                    label="Submit"
+                    type="button"
+                    onClick={handleNext}
+                    ariaLabel="Submit"
+                    variant="ghost"
+                    className="!bg-[#EF8F57] w-fit rounded-sm"
+                />
             </div>
         </div>
     )
--- src/components/inperson_tour_form_steps/StepOne.tsx
+++ src/components/inperson_tour_form_steps/StepOne.tsx
@@ -4,7 +4,8 @@ import { CustomSelect } from "../common/CustomSelect";
 import { countryOptions } from "@/data/countryList";
 import { CustomCheckBox } from "../common/CustomCheckbox";
 import Button from "../common/Button";
-import React, { SetStateAction} from "react";
+import React from "react";
+import toast from "react-hot-toast";
 
 
 
@@ -12,32 +13,22 @@ import React, { SetStateAction} from "react";
 interface StepOneProps {
     formValues: inpersonFormUserData;
     setFormValues: React.Dispatch<React.SetStateAction<inpersonFormUserData>>
-    stepOnePositionX: string;
-    setStepOnePositionX: React.Dispatch<SetStateAction<string>>
+    setStepTwo: () => void
 }
 
 
 
-export const StepOne = ({ formValues, setFormValues, stepOnePositionX, setStepOnePositionX }: StepOneProps) => {
+export const StepOne = ({ formValues, setFormValues, setStepTwo }: StepOneProps) => {
 
 
 
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         const { name, value } = e.target;
-        // const updated = { ...u, [name]: value };
+
         setFormValues((prev) => ({
             ...prev,
             [name]: value
         }));
-
-        // const fieldName = name as keyof userDataType;
-        // const fieldError = validateUserData(updated, fieldName);
-
-        // setFormErrors(prev => {
-        //     const rest = { ...prev };
-        //     delete rest[fieldName];
-        //     return fieldError[fieldName] ? { ...rest, [fieldName]: fieldError[fieldName] } : rest;
-        // });
     };
 
 
@@ -46,15 +37,6 @@ export const StepOne = ({ formValues, setFormValues, stepOnePositionX, setStepOn
     const handleSelectChange = (name: string, value: string) => {
         const updated = { ...formValues, [name]: value };
         setFormValues(updated);
-
-        // const field = name as keyof userDataType;
-        // const fieldError = validateUserData(updated, field);
-
-        // setFormErrors(prev => {
-        //     const rest = { ...prev };
-        //     delete rest[field];
-        //     return fieldError[field] ? { ...rest, [field]: fieldError[field] } : rest;
-        // });
     };
 
 
@@ -68,117 +50,102 @@ export const StepOne = ({ formValues, setFormValues, stepOnePositionX, setStepOn
                 [name]: value,
             });
         }
-
-
-        // const fieldError = validateUserData(updatedUserData, fieldToValidate)
-
-        // setFormErrors((prev) => {
-        //     const rest = { ...prev }
-        //     delete rest[fieldToValidate]
-        //     // Special handling for OtherReason error if 'others' is deselected
-        //     if (fieldToValidate === "reasonForJoin" && !updatedUserData.reasonForJoin.includes("others")) {
-        //         delete rest.OtherReason
-        //     }
-        //     return fieldError[fieldToValidate] ? { ...rest, [fieldToValidate]: fieldError[fieldToValidate] } : rest
-        // })
-
-
     };
 
 
+    const handleNext = () => {
+        if (!formValues.fullName || !formValues.emailAddress || !formValues.phoneNumber || !formValues.country || !formValues.isAdult ) {
+            toast.error("All required fields must be completed!")
+            return;
+        }
+        setStepTwo()
+    }
+
 
     return (
+        <div className=" bg-[#FDF4F1] h-fit w-full max-w-4xl  flex items-center justify-center py-6 px-4 md:px-6 flex-col gap-7 rounded-md  " >
+            <h3 className=" text-2xl font-semibold text-black " >Personal details</h3>
 
-        <div className={`  flex items-center justify-center  h-full w-full px-4 z-50 absolute top-[50%] left-[50%]  translate-y-[-50%] transition-all duration-400 ease-in-out ${stepOnePositionX} `} >
-            <div className=" bg-[#FDF4F1] h-fit w-full max-w-4xl  flex items-center justify-center py-6 px-4 md:px-6 flex-col gap-4 rounded-md  " >
-                <h3 className=" text-2xl font-semibold text-black " >Personal details</h3>
-
-                {/* full name input  */}
-                <Input
-                    value={formValues.fullName}
-                    type="string"
-                    label="Full name"
-                    name="fullName"
-                    onChange={handleChange}
-                    placeholder="John Ade"
-                    isRequired
-                />
+            {/* full name input  */}
+            <Input
+                value={formValues.fullName}
+                type="text"
+                label="Full name"
+                name="fullName"
+                onChange={handleChange}
+                placeholder="John Ade"
+                isRequired
+            />
 
 
-                {/* email address input  */}
-                <Input
-                    value={formValues.emailAddress}
-                    type="string"
-                    label="Email address"
-                    name="emailAddress"
-                    onChange={handleChange}
-                    placeholder="JohnAde@gmail.com"
-                    isRequired
-                />
+            {/* email address input  */}
+            <Input
+                value={formValues.emailAddress}
+                type="text"
+                label="Email address"
+                name="emailAddress"
+                onChange={handleChange}
+                placeholder="JohnAde@gmail.com"
+                isRequired
+            />
 
 
-                {/* phone number input   */}
-                <Input
-                    value={formValues.phoneNumber}
-                    type="string"
-                    label="Phone Number"
-                    name="phoneNumber"
-                    onChange={handleChange}
-                    isRequired
-                />
+            {/* phone number input   */}
+            <Input
+                value={formValues.phoneNumber}
+                type="text"
+                label="Phone Number"
+                name="phoneNumber"
+                onChange={handleChange}
+                isRequired
+            />
 
 
 
-                {/* country input  */}
-                <CustomSelect
-                    name="country"
-                    onChange={handleSelectChange}
-                    options={countryOptions}
-                    label="Country"
-                    placeholder="Please select an option"
-                    //  error={formErrors.country}
-                    isRequired
-                    value={formValues.country}
-                />
+            {/* country input  */}
+            <CustomSelect
+                name="country"
+                onChange={handleSelectChange}
+                options={countryOptions}
+                label="Country"
+                placeholder="Please select an option"
+                isRequired
+                value={formValues.country}
+            />
 
 
 
-                {/* Age input  */}
-                <div className="w-full flex flex-col items-start gap-5 " >
-                    <h1 className="text-[#000000] font-medium text-base font-lato flex items-start gap-1" >Are you 18 years or older?  <div className=" text-red-600" >*</div></h1>
+            {/* Age input  */}
+            <div className="w-full flex flex-col items-start gap-5 " >
+                <h1 className="text-[#000000] font-medium text-base font-lato flex items-start gap-1" >Are you 18 years or older?  <div className=" text-red-600" >*</div></h1>
 
 
-                    <div className=" grid grid-cols-1 md:grid-cols-2 gap-4 justify-items-stretch  "  >
-                        {["yes", "no"].map((option, index) => {
-                            const isChecked = formValues.isAdult === option
-                            return (
-                                <CustomCheckBox
-                                    key={index}
-                                    checked={isChecked}
-                                    onCheckedChange={() => handleCheckboxChange("isAdult", true, option)}
-                                    label={option.toUpperCase()}
-                                    id={option}
-
-                                />
-                            )
-                        })}
-                    </div>
-                    {/* {formErrors.reasonForJoin && (
-                                <p className="text-red-500 text-xs md:text-sm ml-auto ">{formErrors.reasonForJoin}</p>
-                            )} */}
+                <div className=" grid grid-cols-1 md:grid-cols-2 gap-4 justify-items-stretch  "  >
+                    {["yes", "no"].map((option, index) => {
+                        const isChecked = formValues.isAdult === option
+                        return (
+                            <CustomCheckBox
+                                key={index}
+                                checked={isChecked}
+                                onCheckedChange={() => handleCheckboxChange("isAdult", true, option)}
+                                label={option.toUpperCase()}
+                                id={option}
+                            />
+                        )
+                    })}
                 </div>
-
-
-                <Button
-                    label="Next"
-                    type="button"
-                    ariaLabel="next"
-                    variant="ghost"
-                    className="!bg-[#EF8F57] w-fit rounded-sm ml-auto  "
-                    onClick={() => setStepOnePositionX("translate-x-[-400%]") }
-                />
-
             </div>
+
+
+            <Button
+                label="Next"
+                type="button"
+                ariaLabel="next"
+                variant="ghost"
+                className="!bg-[#EF8F57] w-fit rounded-sm ml-auto  "
+                onClick={handleNext}
+            />
+
         </div>
     )
 }
