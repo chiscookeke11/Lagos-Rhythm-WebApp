72aa409b77eea9c6a21799704acccad79d6ce263
chiscookeke11 <chiscookeke11@gmail.com>
2025-12-23 08:46:13 +0100

    chnaged the link

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -150,7 +150,7 @@ export default function Navbar() {
 
 
 
-                {isSignedIn ? (<DropdownMenuCheckboxes />) : <Button onClick={() => setShowAuthModal(true)} label="Sign In" type="button" ariaLabel="Sign in" variant="outline" />}
+                {isSignedIn ? (<DropdownMenuCheckboxes />) : <Button onClick={() => setShowAuthModal(true)} label="Sign In" type="button" ariaLabel="Sign in" variant="outline" className={pathname === "/auth" ? "hidden" : "" } />}
 
 
 
