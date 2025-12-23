e2d36e057d47e4079b2773fa4098dbaaee86c5c7
chiscookeke11 <chiscookeke11@gmail.com>
2025-12-23 08:24:43 +0100

    updates

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -59,14 +59,14 @@ export default function HeroSection({ setVideoLoaded, videoLoaded }: HeroSection
               Live the vibe, please the mind!
             </motion.p>
           </div>
-          <Link href={"/Free_E-Rhythm"} >
+          <a href={"https://www.lagosrhythm.com/exclusive-tour-form"} target="_blank" >
             <MotionButton
               initial={{ scale: 0, opacity: 0 }}
               animate={{ scale: 1, opacity: 1 }}
               transition={{ duration: 0.1, delay: 0.7 }}
               whileHover={{ scale: 1.3 }}
               whileTap={{ scale: 1.3 }}
-              type="button" label="Free Virtual Tour" > </MotionButton></Link>
+              type="button" label=" Detty December Tour" > </MotionButton></a>
         </div>
       </div>
       <ScrollingText />
