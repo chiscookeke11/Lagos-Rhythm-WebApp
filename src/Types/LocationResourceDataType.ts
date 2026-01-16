f0db5ebd9e5d30d36db4cf594e0a37fca339efa9
chiscookeke11 <chiscookeke11@gmail.com>
2026-01-16 14:57:24 +0100

    implemented the direction feature UI and logic

---
+++ src/Types/LocationResourceDataType.ts
@@ -0,0 +1,16 @@
+
+
+export interface LocationResourceDataType {
+    content_url: string,
+    description: string,
+    from_location: string,
+    to_location: string,
+    from_normalized: string,
+    to_normalized: string,
+    from_keywords: string[],
+    to_keywords: string[],
+    tags: string[],
+    type: string,
+    id: string,
+    route_key: string
+}
