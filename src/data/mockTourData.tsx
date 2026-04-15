3c6d9b2311d58bcae201794dbc83cddd93b9c4ae
chiscookeke11 <chiscookeke11@gmail.com>
2026-04-15 19:12:32 +0100

    updated the tour time

--- src/data/mockTourData.tsx
+++ src/data/mockTourData.tsx
@@ -4,7 +4,7 @@
 export const mock_tour_data = {
     date: "2026-04-15T18:00:00+01:00",
     videoUrl: "https://res.cloudinary.com/dwedz2laa/video/upload/v1776275586/u9cvyqpavvjggew1kqy6.mp4",
-    tourTitle: "Born From This Ground begins shortly",
+    tourTitle: "E.D.’s FIRE: Introducing his world",
     tags: [
         "Virtual tourism cuts travel costs by 80%.",
         "Virtual tours make 70% of users feel more confident visiting later.",
